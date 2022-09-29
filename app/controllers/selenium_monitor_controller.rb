4de5c579437842b83106d969d2c94c7a73e62c96
laisy <laisyferreira2011@gmail.com>
2022-09-28 21:14:18 -0300

    Remove looping

--- app/controllers/selenium_monitor_controller.rb
+++ app/controllers/selenium_monitor_controller.rb
@@ -3,35 +3,29 @@ class SeleniumMonitorController < ApplicationController
   require 'selenium-webdriver'
 
   def create
-    while(@flag)
-
-      Link.all.each do |link|
-        date = DateTime.now
-      
-        # @driver = Selenium::WebDriver.for :chrome
-        @driver = Selenium::WebDriver.for ENV['CHROME_DRIVER_PATH'] || :chrome
-        wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-        @driver.get link.url
-      
-        path = "./app/assets/images/#{link.id}#{date}.png"
-      
-        @driver.save_screenshot(path)
-        @driver.quit
-      
-        image = File.new(path, "r")
-      
-        s = Screenshot.new
-        s.link_id = link.id
-        s.image = image
-        s.title = link.url
-        s.save!
-      
-        image.close
-      
-        File.delete(path) if File.exist?(path)
-      end
-
-      sleep(10.seconds)
+    Link.all.each do |link|
+      date = DateTime.now
+    
+      @driver = Selenium::WebDriver.for :chrome
+      wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+      @driver.get link.url
+    
+      path = "./app/assets/images/#{link.id}#{date}.png"
+    
+      @driver.save_screenshot(path)
+      @driver.quit
+    
+      image = File.new(path, "r")
+    
+      s = Screenshot.new
+      s.link_id = link.id
+      s.image = image
+      s.title = link.url
+      s.save!
+    
+      image.close
+    
+      File.delete(path) if File.exist?(path)
     end
   end
 
